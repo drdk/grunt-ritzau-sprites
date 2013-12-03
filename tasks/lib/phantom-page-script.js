62d5a3eeb357e48e95e34e1d375006f2e7a49a0b
rafl <rafl@dr.dk>
2013-12-03 13:49:58 +0100

    doesnt fail on 404 and resizing properly

--- tasks/lib/phantom-page-script.js
+++ tasks/lib/phantom-page-script.js
@@ -1,127 +1,142 @@
 window.onload = function () {
 
-  var images = Array.prototype.slice.call(document.images, 0),
-    count = images.length;
-  
-  images.forEach(function (image) {
-    if (image.complete) {
-      countDown();
-    }
-    else {
-      image.onload = countDown;
-    }
-  });
-  
-  function countDown() {
-    count--;
-    if (!count) {
-      makeSprite();
-    }
-  }
+	var images = $(document.images),
+		count = images.length;
+	
+	images.forEach(function (image) {
+		if (image.complete) {
+			countDown();
+		}
+		else {
+			image.onload = countDown;
+		}
+	});
+	
+	function countDown() {
+		count--;
+		if (!count) {
+			makeSprite();
+		}
+	}
 
 };
 
+function $ (selector) {
+	return Array.prototype.slice.call((typeof selector == "string") ? document.querySelectorAll(selector) : selector, 0);
+}
+
+String.prototype.replaceToken = function(object) {
+	return this.replace(/\{([a-zA-Z]+)\}/g, function (m, key) {
+		return (key in object) ? object[key] : m;
+	});
+};
 
 function makeSprite () {
-  var canvas, channel, channels, css, cssTemplate, ctx, filenamePrefix, height, image, imgs, largestImageWithIdent, logoPath, moo, offset, result, width, _i, _j, _len, _len1;
-  cssTemplate = options.cssTemplate;
-  channels = options.channels;
-  logoPath = options.logoPath;
-  filenamePrefix = options.filenamePrefix;
-  moo = [];
-  String.prototype.replaceObject = function(obj) {
-    var key, re, result;
-    result = this;
-    for (key in obj) {
-      re = new RegExp("{" + key + "}", "g");
-      result = result.replace(re, obj[key]);
-    }
-    return result;
-  };
-  largestImageWithIdent = function(ident) {
-    var image, images, maxWidth, result, _i, _len;
-    maxWidth = 0;
-    images = document.querySelectorAll("img[data-ident='" + ident + "']");
-    result = null;
-    for (_i = 0, _len = images.length; _i < _len; _i++) {
-      image = images[_i];
-      if (image.width > maxWidth) {
-        maxWidth = image.width;
-        result = image;
-      }
-    }
-    return result;
-  };
-  width = 0;
-  height = 0;
-  css = "";
-  imgs = [];
-  for (_i = 0, _len = channels.length; _i < _len; _i++) {
-    channel = channels[_i];
-    image = largestImageWithIdent(channel.ident);
-    if (image) {
-      if (!(image.height >= 30)) {
-        moo.push(channel.ident);
-      }
-      channel.spriteImage = image;
-      if (!(width >= image.width)) {
-        width = image.width;
-      }
-      height += image.height;
-    }
-  }
-  canvas = document.getElementById("sprite");
-  ctx = canvas.getContext("2d");
-  canvas.width = width;
-  canvas.height = height;
-  offset = 0;
-  for (_j = 0, _len1 = channels.length; _j < _len1; _j++) {
-    channel = channels[_j];
-    if (!channel.spriteImage) {
-      continue;
-    }
-    try {
-      ctx.drawImage(channel.spriteImage, 0, offset);
-      imgs.push(channel.spriteImage.src.replace("file://", ""));
-      offset += channel.spriteImage.height;
-      css += "\n\n" + cssTemplate.replaceObject({
-        name: "" + filenamePrefix + "-" + channel.ident,
-        width: channel.spriteImage.width,
-        height: channel.spriteImage.height,
-        pos: offset - channel.spriteImage.height,
-        logoName: "" + filenamePrefix + "-" + channel.ident + ".png",
-        logoPath: logoPath
-      });
-    }
-    catch (e) {
+	var canvas, css, ctx, height, imgs, largestImageWithIdent, moo, offset, result, width,
+		cssTemplate = options.cssTemplate,
+		channels = options.channels,
+		logoPath = options.logoPath,
+		filenamePrefix = options.filenamePrefix,
+		maxWidth = 110,
+		maxHeight = 35,
+		minWidth = 40,
+		minHeight = 20;
 
-    }
-  }
+	moo = [];
 
-  var images = Array.prototype.slice.call(document.images, 0);
-  images.forEach(function (image) {
-    image.parentNode.removeChild(image);
-  });
+	largestImageWithIdent = function(ident) {
+		var images = $("img[data-ident='" + ident + "']"),
+			maxWidth = 0,
+			result;
+		images.forEach(function (image) {
+			if (image.width > maxWidth) {
+				maxWidth = image.width;
+				result = image;
+			}
+		});
+		return result;
+	};
 
-  result = {
-    /*
-    sprite: JSON.stringify(canvas.toDataURL({
-      format: "png"
-    })),
-    */
-    css: css,
-    width: width,
-    height: height,
-    images: imgs
-  };
-  
-  result = JSON.stringify(result);
+	function getSize (image) {
+		var wfactor = maxWidth / image.width,
+			yfactor = maxHeight / image.height,
+			factor = Math.min(wfactor, yfactor);
 
-  if (typeof window.callPhantom === 'function') {
-    window.callPhantom(result);
-  }
-  else if (window.console) {
-    console.log(result);
-  }
-  
-};
+		if (image.width < minWidth && image.height < minHeight) {
+			factor = 2;
+		}
+
+		return {
+			width: Math.ceil(image.width * factor),
+			height: Math.ceil(image.height * factor)
+		};
+	}
+
+	width = 0;
+	height = 0;
+	css = "";
+	imgs = [];
+
+	channels.forEach(function (channel) {
+		var image = largestImageWithIdent(channel.ident);
+		if (image) {
+			if (image.height < 30) {
+				moo.push(channel.ident);
+			}
+			channel.spriteImage = image;
+			channel.size = getSize(image);
+			if (width < channel.size.width) {
+				width = channel.size.width;
+			}
+			height += channel.size.height;
+		}
+	});
+
+	canvas = document.getElementById("sprite");
+	ctx = canvas.getContext("2d");
+	canvas.width = width;
+	canvas.height = height;
+	offset = 0;
+
+	channels.forEach(function (channel) {
+		if (channel.spriteImage) {
+			try {
+				ctx.drawImage(channel.spriteImage, 0, offset, channel.size.width, channel.size.height);
+				imgs.push(channel.spriteImage.src.replace("file://", ""));
+				offset += channel.size.height;
+				css += "\n\n" + cssTemplate.replaceToken({
+					name: "" + filenamePrefix + "-" + channel.ident,
+					width: channel.size.width,
+					height: channel.size.height,
+					pos: offset - channel.size.height,
+					logoName: "" + filenamePrefix + "-" + channel.ident + ".png",
+					logoPath: logoPath
+				});
+			}
+			catch (e) {
+			}
+		}
+	});
+
+	var images = Array.prototype.slice.call(document.images, 0);
+	images.forEach(function (image) {
+		image.parentNode.removeChild(image);
+	});
+
+	result = {
+		css: css,
+		width: width,
+		height: height,
+		images: imgs
+	};
+	
+	result = JSON.stringify(result);
+
+	if (typeof window.callPhantom === 'function') {
+		window.callPhantom(result);
+	}
+	else if (window.console) {
+		console.log(result);
+	}
+	
+}
--- tasks/lib/phantom-script.js
+++ tasks/lib/phantom-script.js
@@ -1,35 +1,35 @@
 var fs = require("fs"),
-  webpage = require("webpage"),
-  system = require("system");
+	webpage = require("webpage"),
+	system = require("system");
 
 phantom.onError = function (msg, trace) {
-  var msgStack = ["PHANTOM ERROR: " + msg];
-  if (trace && trace.length) {
-    msgStack.push("TRACE:");
-    trace.forEach(function (t) {
-      Stack.push(" -> " + (t.file || t.sourceURL) + ": " + t.line + (t.function ? " (in function " + t.function + ")" : ""));
-    });
-  }
-  system.stderr.write(msgStack.join("\n"));
-  phantom.exit(1);
+	var msgStack = ["PHANTOM ERROR: " + msg];
+	if (trace && trace.length) {
+		msgStack.push("TRACE:");
+		trace.forEach(function (t) {
+			Stack.push(" -> " + (t.file || t.sourceURL) + ": " + t.line + (t.function ? " (in function " + t.function + ")" : ""));
+		});
+	}
+	system.stderr.write(msgStack.join("\n"));
+	phantom.exit(1);
 };
 
 var url = system.args[1],
-  output = system.args[2],
-  page = webpage.create();
+	output = system.args[2],
+	page = webpage.create();
 
 page.onCallback = function (response) {
 
-  var size = page.evaluate(function () {
-    return {width: sprite.offsetWidth, height: sprite.offsetHeight};
-  });
+	var size = page.evaluate(function () {
+		return {width: sprite.offsetWidth, height: sprite.offsetHeight};
+	});
 
-  page.viewportSize = size;
+	page.viewportSize = size;
 
-  page.render(output);
-  page.close();
-  system.stdout.write(response);
-  phantom.exit();
+	page.render(output);
+	page.close();
+	system.stdout.write(response);
+	phantom.exit();
 
 };
 
